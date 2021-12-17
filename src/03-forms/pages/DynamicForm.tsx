fa75f90ae436245f241cc9f96b0de8cb3c636da2
Adrián Antón <adrtler@gmail.com>
2021-12-17 17:48:11 +0100

    Select de manera dinámica

--- src/03-forms/pages/DynamicForm.tsx
+++ src/03-forms/pages/DynamicForm.tsx
@@ -1,6 +1,7 @@
 import formJson from '../data/custom-form.json';
 import { Formik, Form } from 'formik';
 import { MyTextInput } from '../components';
+import { MySelect } from '../components/MySelect';
 
 const initalValues: { [x: string]: any } = {};
 
@@ -20,16 +21,31 @@ export const DynamicForm = () => {
       >
         {() => (
           <Form noValidate>
-            {formJson.map(({ type, name, placeholder, label }) => {
-              return (
-                <MyTextInput
-                  key={name}
-                  name={name}
-                  label={label}
-                  type={type as any}
-                  placeholder={placeholder}
-                />
-              );
+            {formJson.map(({ type, name, placeholder, label, options }) => {
+              if (type === 'input' || type === 'password' || type === 'email') {
+                return (
+                  <MyTextInput
+                    key={name}
+                    name={name}
+                    label={label}
+                    type={type as any}
+                    placeholder={placeholder}
+                  />
+                );
+              } else if (type === 'select') {
+                return (
+                  <MySelect key={name} label={label} name={name}>
+                    <option value="">Select an option</option>
+                    {options?.map(({ id, label }) => (
+                      <option key={id} value={id}>
+                        {label}
+                      </option>
+                    ))}
+                  </MySelect>
+                );
+              }
+
+              throw new Error(`El type ${type} no es soportado`);
             })}
             <button type="submit">Submit</button>
           </Form>
