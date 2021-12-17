bf6d71a84d2993f0c90c6ea48a54c8804c245b13
Adrián Antón <adrtler@gmail.com>
2021-12-17 09:42:15 +0100

    Formik - selects and checks

--- src/03-forms/pages/FormikComponentsPage.tsx
+++ src/03-forms/pages/FormikComponentsPage.tsx
@@ -1,4 +1,4 @@
-import { useFormik, Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import '../styles/styles.css';
 
@@ -7,7 +7,13 @@ export const FormikComponentsPage = () => {
     <div>
       <h1>Formik Components tutorial</h1>
       <Formik
-        initialValues={{ firstName: '', lastName: '', email: '' }}
+        initialValues={{
+          firstName: '',
+          lastName: '',
+          email: '',
+          terms: false,
+          jobType: '',
+        }}
         onSubmit={(values) => {
           console.log(values);
         }}
@@ -21,6 +27,13 @@ export const FormikComponentsPage = () => {
           email: Yup.string()
             .email('El formato del email no es correcto')
             .required('Requerido'),
+          terms: Yup.boolean()
+            .oneOf([true], 'Must accept the terms and conditions')
+            .required('Requerido'),
+          // Bloqueamos una opción con notOneOf
+          jobType: Yup.string()
+            .notOneOf(['it-jr'], 'Esta opción no es permitida')
+            .required('Required'),
         })}
       >
         {(formik) => (
@@ -40,23 +53,26 @@ export const FormikComponentsPage = () => {
             <label htmlFor="email">Email Address</label>
             <Field name="email" type="email" />
             <ErrorMessage name="email" component="span" />
+
+            <label htmlFor="jobType">Job Type</label>
+            <Field name="jobType" as="select">
+              <option value="">Pick something</option>
+              <option value="developer">Developer</option>
+              <option value="designer">Designer</option>
+              <option value="it-senior">IT senior</option>
+              <option value="it-jr">IT junior</option>
+            </Field>
+            <ErrorMessage name="jobType" component="span" />
+
+            <label>
+              <Field name="terms" type="checkbox" />
+              Terms and conditions
+            </label>
+            <ErrorMessage name="terms" component="span" />
             <button type="submit">Submit</button>
           </Form>
         )}
       </Formik>
-      {/* <form onSubmit={handleSubmit} noValidate>
-        <label htmlFor="firstName">First name</label>
-        <input type="text" {...getFieldProps('firstName')} />
-        {touched.firstName && firstNameError && <span>{firstNameError}</span>}
-
-        <label htmlFor="lastName">Last name</label>
-        <input type="text" {...getFieldProps('lastName')} />
-        {touched.lastName && lastNameError && <span>{lastNameError}</span>}
-        <label htmlFor="email">Email Address</label>
-        <input type="email" {...getFieldProps('email')} />
-        {touched.email && emailError && <span>{emailError}</span>}
-        <button type="submit">Enviar</button>
-      </form> */}
     </div>
   );
 };
